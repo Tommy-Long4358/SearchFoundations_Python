22baeaca95d0f53c57f1db7bfc7cfe2cb60a3a33
Crimson5768 <tommy.long7548@gmail.com>
2023-11-20 12:39:51 -0800

    corrected AND querying

--- querying/andquery.py
+++ querying/andquery.py
@@ -19,33 +19,24 @@ class AndQuery(QueryComponent):
     contained in the AndQuery have been merged.
     '''
     def get_postings(self, index : Index) -> list[Posting]:
-        # TODO: program the merge for an AndQuery, by gathering the postings of the composed QueryComponents and
-		# intersecting the resulting postings.
         results = []
-
+        
+        # Starting posting list
         recent_term_posting_list = self.components[0].get_postings(index)
 
         for i in range(1, len(self.components)):
+            # ith posting list to compare to starting posting list
             next_posting_list = self.components[i].get_postings(index)
 
             index1, index2 = 0, 0
-
+            
             while index1 < len(recent_term_posting_list) and index2 < len(next_posting_list):
                 # Check if IDs are equal to each other or not
                 if recent_term_posting_list[index1].doc_id == next_posting_list[index2].doc_id:
-                    new_position_list = []
-
-                    for position1 in recent_term_posting_list[index1].position_list:
-                        for position2 in next_posting_list[index2].position_list:
-                            if position1 not in new_position_list:
-                                new_position_list.append(position1)
-                            
-                            if position2 not in new_position_list:
-                                new_position_list.append(position2)
-
-                    if len(new_position_list) > 0:
-                        #print(recent_term_posting_list[index1].doc_id, new_position_list)
-                        results.append(Posting(recent_term_posting_list[index1].doc_id, new_position_list))
+                    new_position_list = sorted(recent_term_posting_list[index1].position_list + next_posting_list[index2].position_list)
+                    
+                    #TODO: What to do with position list?? AND all positions?
+                    results.append(Posting(recent_term_posting_list[index1].doc_id, new_position_list))
 
                     index1 += 1
                     index2 += 1
@@ -57,8 +48,9 @@ class AndQuery(QueryComponent):
                 else:
                     index1 += 1
 
+            # Only update starting posting list if results were found for both terms
             if len(results) > 1:
-                # Update posting list to be compared 
+                # Set starting posting list to merged posting list
                 recent_term_posting_list = results[len(results) - 1]
         
         return results
